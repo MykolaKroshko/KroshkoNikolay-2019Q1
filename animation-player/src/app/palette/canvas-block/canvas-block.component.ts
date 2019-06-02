61f4942c65ae80599e730ddb8b7dc22b9ae640fd
KroshkoNikolay <xamjofoux@gmail.com>
2019-06-02 17:35:34 +0300

    feat: display frame preview in frames carousel

--- animation-player/src/app/palette/canvas-block/canvas-block.component.ts
+++ animation-player/src/app/palette/canvas-block/canvas-block.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'canvas-block',
   templateUrl: './canvas-block.component.html',
   styleUrls: [ './canvas-block.component.scss' ]
 })
-export class CanvasBlockComponent {
+export class CanvasBlockComponent implements AfterViewInit{
+  @ViewChild('myCanvas') myCanvas: ElementRef;
+  public context: CanvasRenderingContext2D;
 
+  @Input() set picture(pic: object[]){
+    Promise.resolve(null).then(() =>
+      this.drawPicture(pic)
+    );
+  };
+  @Input() public size: number = 0;
+  
+  ngAfterViewInit(): void {
+    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+  }
+
+  private drawPicture(pic) {
+    const len = pic.length;
+    const sideLen = this.size/len;
+    pic.map((row, i) => {
+      row.map((color, j) => {
+        this.context.fillStyle = color;
+        this.context.fillRect(
+          i * sideLen, 
+          j * sideLen, 
+          (i + 1) * sideLen, 
+          (j + 1) * sideLen
+        );
+      })
+    })
+  }
 }
